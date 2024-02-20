340a305d0dbd2968cc2a1101879cf96c4af4c1b7
Sthembiso Ndhlazi <101163890+SthembisoNdhlazi@users.noreply.github.com>
2024-02-20 18:47:56 +0200

    Make Item public

--- Sources/ReusableComponents/ItemListView/Item.swift
+++ Sources/ReusableComponents/ItemListView/Item.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct Item: ItemViewable, Identifiable {
-    public var id: String = UUID().uuidString
+    public var id: String
     
     var drinkName: String
     
@@ -21,4 +21,14 @@ public struct Item: ItemViewable, Identifiable {
     var category: String?
     
     var isFavorite: Bool?
+    
+    public init(id: String, drinkName: String, glass: String? = nil, instructions: String? = nil, image: String, category: String? = nil, isFavorite: Bool? = nil) {
+        self.id = id
+        self.drinkName = drinkName
+        self.glass = glass
+        self.instructions = instructions
+        self.image = image
+        self.category = category
+        self.isFavorite = isFavorite
+    }
 }
