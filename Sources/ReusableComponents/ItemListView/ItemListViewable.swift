68e310745407c641ca2f04b72b5afbf0c2ef1169
Sthembiso Ndhlazi <101163890+SthembisoNdhlazi@users.noreply.github.com>
2024-02-20 19:07:55 +0200

    Add itemDetailView protocol

--- Sources/ReusableComponents/ItemListView/ItemListViewable.swift
+++ Sources/ReusableComponents/ItemListView/ItemListViewable.swift
@@ -7,24 +7,6 @@ public protocol ItemListViewable: ObservableObject {
     func setUpData()
 }
 
-//public struct ItemListViewModel {
-//    public var imageURLString: String
-//    public var title: String
-//    public var subtitle: String
-//    public var id: UUID = UUID()
-//    public var isFavourite: Bool?
-//    
-//    static func == (lhs: ItemListViewModel, rhs: ItemListViewModel) -> Bool {
-//        lhs.id == rhs.id
-//    }
-//    func hash(into hasher: inout Hasher) {
-//        hasher.combine(id)
-//    }
-//    
-//    public init(imageURLString: String, title: String, subtitle: String, isFavorite: Bool? = nil) {
-//        self.imageURLString = imageURLString
-//        self.title = title
-//        self.subtitle = subtitle
-//        self.isFavourite = isFavorite
-//    }
-//}
+public protocol ItemDetailView {
+    var selectedItem: Item { get }
+}
