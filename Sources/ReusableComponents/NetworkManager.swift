6af138268ce9f4cf0f0c121cd6a97f1399eac604
Sthembiso Ndhlazi <ndhlazisthe@gmail.com>
2025-04-07 20:05:50 +0200

    Add network manager class for the generic json fetcher funciton

---
+++ Sources/ReusableComponents/NetworkManager.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+public enum NetworkError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case invalidResponse
+    case invalidData
+}
+
+public class NetworkManager {
+    public func fetchJSON<T: Decodable>(from url: URL, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.requestFailed(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(.requestFailed(error)))
+            }
+        }.resume()
+    }
+}
